db8e800a6e3947e17fb69d194c987d41416cbf77
boseriddha <boseriddha@gmail.com>
2023-06-28 07:40:59 +0530

    registration backend completed

--- api/index.js
+++ api/index.js
@@ -11,21 +11,15 @@ mongoose.connect(
   "mongodb+srv://blog:ik01cO3qlF4v0gCf@cluster0.xqxamrz.mongodb.net/?retryWrites=true&w=majority"
 );
 
-app.post("/register", (req, res) => {
+app.post("/register", async (req, res) => {
   console.log(req.body);
   const { username, password } = req.body;
-  //   try {
-  //     const userDoc = await UserModel.create({ username, password });
-  //     res.json(userDoc);
-  //   } catch (e) {
-  //     res.status(400).json(e);
-  //   }
-  User.create({
-    username: username,
-    password: password,
-  })
-    .then((user) => res.status(201).json(user))
-    .catch((err) => res.status(400).json(err));
+  try {
+    const userDoc = await User.create({ username, password });
+    res.status(201).json(userDoc);
+  } catch (e) {
+    res.status(400).json(e);
+  }
 });
 
 app.listen(4000);
