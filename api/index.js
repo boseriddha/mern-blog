bb0d32e38e1397000b2697c667f45a8d22a53b73
boseriddha <boseriddha@gmail.com>
2023-06-29 16:30:55 +0530

    improved login logout functionality using context

--- api/index.js
+++ api/index.js
@@ -5,12 +5,14 @@ const User = require("./models/User");
 const app = express();
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 
 const salt = bcrypt.genSaltSync();
-const secret = bcrypt.genSaltSync();
+const secret = "asdfaklsdjfasdincvasdlkjfhbvasdfkjasdfasdfoiegjhskdbfalsjkdfh";
 
-app.use(cors());
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 app.use(express.json());
+app.use(cookieParser());
 
 mongoose.connect(
   "mongodb+srv://blog:ik01cO3qlF4v0gCf@cluster0.xqxamrz.mongodb.net/?retryWrites=true&w=majority"
@@ -40,12 +42,27 @@ app.post("/login", async (req, res) => {
     // logged in
     jwt.sign({ username, id: userDoc._id }, secret, {}, (err, token) => {
       if (err) throw err;
-      res.cookie("token", token).json("ok");
+      res.cookie("token", token).json({
+        id: userDoc._id,
+        username: username,
+      });
     });
     // res.status(201).json("ok");
   } else {
     res.status(400).json("wrong credentials");
   }
+});
+
+app.get("/profile", (req, res) => {
+  const { token } = req.cookies;
+  jwt.verify(token, secret, {}, (err, info) => {
+    if (err) throw err;
+    res.json(info);
+  });
+});
+
+app.post("/logout", (req, res) => {
+  res.cookie("token", "").json("ok");
 });
 
 app.listen(4000);
