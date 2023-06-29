5eaf9a7bc2295ba2b5f6d6edaf0a41252d7c0db0
boseriddha <boseriddha@gmail.com>
2023-06-29 21:14:56 +0530

    added single post functionality for front end

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -1,12 +1,17 @@
+import { useState, useEffect } from "react";
 import Post from "./Post";
 
 const Page = () => {
+  const [posts, setPosts] = useState("");
+  useEffect(() => {
+    const response = fetch("http://localhost:4000/post").then((response) => {
+      response.json().then((posts) => {
+        setPosts(posts);
+      });
+    });
+  }, []);
   return (
-    <div>
-      <Post />
-      <Post />
-      <Post />
-    </div>
+    <div>{posts.length > 0 && posts.map((post) => <Post {...post} />)}</div>
   );
 };
 
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,27 +1,24 @@
-const Post = () => {
+import { format } from "date-fns";
+
+const Post = ({ title, summary, cover, content, createdAt, author }) => {
   return (
     <div className="post grid grid-cols-[.9fr_1.1fr] gap-[20px] mb-7">
       <div className="flex justify-center items-center">
         <img
-          src="https://cdn.vox-cdn.com/thumbor/hpncNvQsR4aII8oGo8GHGufsd-c=/0x0:3765x2896/920x613/filters:focal(1582x1147:2184x1749):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/72404511/1258930452.0.jpg"
-          alt=""
+          src={`http://localhost:4000/${cover}`}
+          alt="cover image"
           className="max-w-[100%]"
         />
       </div>
       <div className="text">
-        <h2 className="text-xl font-bold m-0">
-          What’s going on with Vladimir Putin after the mutiny?
-        </h2>
+        <h2 className="text-xl font-bold m-0">{title}</h2>
         <p className="my-[6px] mx-0 text-xs font-bold text-[#888] flex gap-[10px]">
           <a href="#" className="text-[#333]">
-            Riddha Bose
+            {author.username}
           </a>
-          <time>2023-06-23 08:02</time>
+          <time>{format(new Date(createdAt), "MMM d, yyyy HH:mm")}</time>
         </p>
-        <p className="">
-          Putin is still very much in power, but Wagner’s uprising showed the
-          cracks in the regime.
-        </p>
+        <p className="">{summary}</p>
       </div>
     </div>
   );
