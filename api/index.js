8139db2e9fe0c1c081860635a895966b4318b103
boseriddha <boseriddha@gmail.com>
2023-06-29 09:15:49 +0530

    login logic completed

--- api/index.js
+++ api/index.js
@@ -3,6 +3,11 @@ const cors = require("cors");
 const { default: mongoose } = require("mongoose");
 const User = require("./models/User");
 const app = express();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const salt = bcrypt.genSaltSync();
+const secret = bcrypt.genSaltSync();
 
 app.use(cors());
 app.use(express.json());
@@ -12,16 +17,35 @@ mongoose.connect(
 );
 
 app.post("/register", async (req, res) => {
-  console.log(req.body);
   const { username, password } = req.body;
   try {
-    const userDoc = await User.create({ username, password });
+    const userDoc = await User.create({
+      username,
+      password: bcrypt.hashSync(password, salt),
+    });
     res.status(201).json(userDoc);
   } catch (e) {
     res.status(400).json(e);
   }
 });
 
-app.listen(4000);
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  console.log(req.body);
+  const userDoc = await User.findOne({ username });
+  console.log(userDoc);
+  const passOk = bcrypt.compareSync(password, userDoc.password);
+  console.log(passOk);
+  if (passOk) {
+    // logged in
+    jwt.sign({ username, id: userDoc._id }, secret, {}, (err, token) => {
+      if (err) throw err;
+      res.cookie("token", token).json("ok");
+    });
+    // res.status(201).json("ok");
+  } else {
+    res.status(400).json("wrong credentials");
+  }
+});
 
-//mongodb+srv://blog:ik01cO3qlF4v0gCf@cluster0.xqxamrz.mongodb.net/?retryWrites=true&w=majority
+app.listen(4000);
