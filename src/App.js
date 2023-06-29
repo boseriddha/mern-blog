4ae377c82d032b4e567663aeff37ce1886e985a4
boseriddha <boseriddha@gmail.com>
2023-06-29 19:24:19 +0530

    added create-post functionality for front end

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import "./App.css";
 import Page from "./components/Page";
 import Layout from "./components/Layout";
 import LoginPage from "./components/LoginPage";
-import { Routes, Route } from "react-router-dom";
 import RegisterPage from "./components/RegisterPage";
+import CreatePost from "./components/CreatePost";
+import { Routes, Route } from "react-router-dom";
 import { UserContextProvider } from "./hooks/userContext";
 
 function App() {
@@ -14,7 +15,7 @@ function App() {
           <Route index element={<Page />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
-          {/* <Route path="/create" element={<CreatePost />} /> */}
+          <Route path="/create" element={<CreatePost />} />
         </Route>
       </Routes>
     </UserContextProvider>
